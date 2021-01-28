0dbe5d341b36445db24a61c20451ccbb777aaf1a
Kerron Donawa <kerrondonawa@gmail.com>
2021-01-28 18:11:40 -0400

    Added url generation based on encrypted message

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ document.querySelector('form').addEventListener('submit', event=>{
 
     const input = document.querySelector('#message-input');
     const encrypted = btoa(input.value);
+
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
+    //document.execCommand("copy");
 })
